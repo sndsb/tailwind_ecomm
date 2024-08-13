7633d47736a97698810475111e15b280bf5c6901
sndsb <sandeshbudha.whive@gmail.com>
2024-08-13 22:04:04 +0545

    customer review

--- src/sections/CustomerReview.jsx
+++ src/sections/CustomerReview.jsx
@@ -1,6 +1,21 @@
+import ReviewCard from '../components/ReviewCard'
+import {reviews} from '../constants'
 const CustomerReview = () => {
   return (
-    <div>CustomerReview</div>
+    <section className="max-container">
+      <h3 className="font-palanquin text-center text-4xl font-bold">What Our
+        <span className="text-coral-red">Customers</span> Say ?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center justify-evenly">info
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat suscipit facere libero quas eveniet exercitationem, cumque impedit sit, c
+      </p>
+
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {reviews.map((review)=>(
+            <ReviewCard key={review.customerName} {...review} />
+        ))}
+      </div>
+    </section>
   )
 }
 
