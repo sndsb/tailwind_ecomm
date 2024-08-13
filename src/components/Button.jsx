7b877ed0b21dcea199f07b3d0e5238fa05447912
sndsb <sandeshbudha.whive@gmail.com>
2024-08-13 21:36:12 +0545

    special offer

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const Button = ({label,iconUrl}) => {
+const Button = ({label,iconUrl,backgroundColor,textColor,borderColor}) => {
   return (
-    <button className='flex justify-center items-center gap-2 px-7
-     py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red'>
+    <button className={`flex justify-center items-center gap-2 px-7
+     py-4 border font-montserrat text-lg leading-none ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : "bg-coral-red  text-white border-coral-red"}rounded-full w-full} `}>
         {label}
 
         <img src={iconUrl} alt='arrow right icon' className='ml-2 rounded-full w-5 h-5' />
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,6 +1,38 @@
+import { arrowRight } from '../assets/icons'
+import { offer } from '../assets/images'
+import Button from '../components/Button'
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div>
+        <img src={offer} height={687} width={773} className='object-contain  w-full' />
+      </div>
+
+      <div className="flex flex-1 flex-col ">
+
+        <h2 className="font-palanquin font-bold text-4xl capitalize lg:max-w-lg">
+
+            Special <span
+            className="text-coral-red ">
+
+            OFfer
+          </span>
+         
+        </h2>
+
+        <p className="mt-4 lg:max-w-lg info-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.facere amet sunt deserunt doloremque. Voluptate.
+        </p>
+        <div className="mt-11 flex flex-wrap gap-4">
+
+          <Button label="Shop Now" iconUrl={arrowRight} />
+          <Button label="Learn More" iconUrl={arrowRight} 
+          
+           backgroundColor="bg-white" borderColor='border-slate-gray' textColor='text-slate-gray'
+          />
+        </div>
+      </div>
+    </section>
   )
 }
 
