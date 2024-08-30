8556a991ae3f8cc2fff0353dc1c716cf3e9c3b18
sndsb <sandeshbudha.whive@gmail.com>
2024-08-30 18:17:07 +0545

    otp enter page

--- src/pages/auth/Otp.jsx
+++ src/pages/auth/Otp.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import OtpInput from 'react-otp-input';
 
 const Otp = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+
+    const [otp, setOtp] = useState('');
+    
+    return (
+        <div className='main-container padding'>
+            <OtpInput
+                value={otp}
+                onChange={setOtp}
+                numInputs={4}
+                renderSeparator={<span>-</span>}
+                renderInput={(props) => <input {...props} />}
+            />
+
+            <button className='bg-red-400 text-white w-50 h-50'>submit</button>
+        </div>
+    )
 }
 
 export default Otp
